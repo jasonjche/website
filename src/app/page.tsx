4863356658ef0994d1d17ee2743ec2a0c7a65b56
Jason He <jasonhe.md@gmail.com>
2023-06-12 20:08:38 -0700

    Add basic CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Jason He</h1>
-      <p>Hi, I'm Jason He, and I'm a product management intern at Reddit</p>
-      <p>I'm a rising senior at the University of Virginia studying Computer Science and Commerce</p>
-      <p>Previously I've interned at Stacker, Viget, and Parenthood Ventures</p>
-      <p>Here is my portfolio</p>
-      <p>Here is my resume</p>
-      <p>Here is my <a href="mailto:jasonjche@virginia.edu">email</a></p>
-      <p>Here is my <a href="https://www.linkedin.com/in/jasonjche/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
-      <p>Here is my <a href="https://github.com/jasonjche/" target="_blank" rel="noopener noreferrer">Github</a></p>
-      <p>Here is my <a href="https://twitter.com/jasonjche/" target="_blank" rel="noopener noreferrer">Twitter</a></p>
-      <p>Reach out to me if you are interested in chatting or have SWE/PM opportunities!</p>
+    <div className="max-w-2xl mx-auto p-4 text-center">
+      <h1 className="text-4xl font-bold mb-4">Jason He</h1>
+      <p className="text-lg mb-4">Hi, I'm Jason He, and I'm a product management intern at Reddit</p>
+      <p className="text-lg mb-4">I'm an undergraduate at the University of Virginia studying Computer Science and Commerce</p>
+      <p className="text-lg mb-4">Previously I've interned at Stacker, Viget, and Parenthood Ventures</p>
+      <p className="text-blue-500 mb-2">Here is my portfolio</p>
+      <p className="text-blue-500 mb-2">Here is my resume</p>
+      <p className="text-blue-500 mb-2">Here is my <a href="mailto:jasonjche@virginia.edu" target="_blank">email</a></p>
+      <p className="text-blue-500 mb-2">Here is my <a href="https://www.linkedin.com/in/jasonjche/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+      <p className="text-blue-500 mb-2">Here is my <a href="https://github.com/jasonjche/" target="_blank" rel="noopener noreferrer">Github</a></p>
+      <p className="text-blue-500 mb-2">Here is my <a href="https://twitter.com/jasonjche/" target="_blank" rel="noopener noreferrer">Twitter</a></p>
+      <p className="text-lg">Reach out to me if you are interested in chatting or have SWE/PM opportunities!</p>
     </div>
-  )
+  );
 }
