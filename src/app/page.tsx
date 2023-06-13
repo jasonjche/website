d7c1f94968adcb4e92a5b378fdd303b2eb117a3d
Jason He <jasonhe.md@gmail.com>
2023-06-12 20:59:22 -0700

    Redesign layout and change color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,43 @@
 export default function Home() {
   return (
-    <div className="max-w-2xl mx-auto p-4 text-center">
-      <h1 className="text-4xl font-extrabold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Jason He</h1>
-      <p className="text-lg mb-4">Hi, I'm Jason He, and I'm a product management intern at Reddit</p>
-      <p className="text-lg mb-4">I'm a rising senior at the University of Virginia studying Computer Science and Commerce</p>
-      <p className="text-lg mb-4">Previously I've interned at Stacker, Viget, and Parenthood Ventures</p>
-      <p className="text-blue-500 mb-2">Here is my portfolio</p>
-      <p className="text-blue-500 mb-2">Here is my resume</p>
-      <p className="text-blue-500 mb-2">Here is my <a href="mailto:jasonjche@virginia.edu" target="_blank">email</a></p>
-      <p className="text-blue-500 mb-2">Here is my <a href="https://www.linkedin.com/in/jasonjche/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
-      <p className="text-blue-500 mb-2">Here is my <a href="https://github.com/jasonjche/" target="_blank" rel="noopener noreferrer">Github</a></p>
-      <p className="text-blue-500 mb-2">Here is my <a href="https://twitter.com/jasonjche/" target="_blank" rel="noopener noreferrer">Twitter</a></p>
-      <p className="text-lg">Reach out to me if you are interested in chatting or have SWE/PM opportunities!</p>
-    </div>
+    <body className="max-w-7xl mx-auto p-8 bg-dawn-bg">
+      <h1 className="text-4xl font-bold mb-4 text-dawn-text">Jason He</h1>
+      <h2 className="text-2xl font-medium mb-4 text-dawn-text">Product Management Intern at Reddit</h2>
+      <h3 className="text-1xl font-light mb-4 text-dawn-text">University of Virginia | Computer Science and Commerce</h3>
+
+      <div className="grid grid-cols-2 gap-4 text-dawn-text">
+        <div>
+          <h2 className="text-2xl font-medium mb-2">Experience</h2>
+          <p className="mb-2">Stacker</p>
+          <p className="mb-2">Viget</p>
+          <p>Parenthood Ventures</p>
+        </div>
+
+        <div>
+          <h2 className="text-2xl font-medium mb-2">Contact</h2>
+          <p className="text-dawn-accent mb-2">
+            <a href="mailto:jasonjche@virginia.edu" target="_blank" className="text-dawn-accent">Email</a>
+          </p>
+          <p className="text-dawn-accent mb-2">
+            <a href="https://www.linkedin.com/in/jasonjche/" target="_blank" rel="noopener noreferrer" className="text-dawn-accent">LinkedIn</a>
+          </p>
+          <p className="text-dawn-accent mb-2">
+            <a href="https://github.com/jasonjche/" target="_blank" rel="noopener noreferrer" className="text-dawn-accent">Github</a>
+          </p>
+          <p className="text-dawn-accent mb-2">
+            <a href="https://twitter.com/jasonjche/" target="_blank" rel="noopener noreferrer" className="text-dawn-accent">Twitter</a>
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-4 text-dawn-text">
+        <h2 className="text-2xl font-medium mb-2">Looking for opportunities</h2>
+        <p className="text-lg">Reach out to me if you are interested in chatting or have SWE/PM opportunities!</p>
+      </div>
+
+      <div className="mt-8 text-center">
+        <a href="https://www.dropbox.com/s/za6ptynh53kt8bc/Jason%20He%20Resume%20-%20Product.pdf?dl=0" className="text-dawn-accent text-lg">Here is my resume</a>
+      </div>
+    </body>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,12 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      'dawn-bg': '#2A222E',
+      'dawn-text': '#EEEFF1',
+      'dawn-accent': '#A84376',
     },
   },
+  variants: {},
   plugins: [],
 }
