5db4c683eda7e6e8657f6bcaf26fddfded9b7d5f
Jason He <jasonhe.md@gmail.com>
2023-06-12 20:20:57 -0700

    Make gradient header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <div className="max-w-2xl mx-auto p-4 text-center">
-      <h1 className="text-4xl font-bold mb-4">Jason He</h1>
+      <h1 className="text-4xl font-extrabold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Jason He</h1>
       <p className="text-lg mb-4">Hi, I'm Jason He, and I'm a product management intern at Reddit</p>
-      <p className="text-lg mb-4">I'm an undergraduate at the University of Virginia studying Computer Science and Commerce</p>
+      <p className="text-lg mb-4">I'm a rising senior at the University of Virginia studying Computer Science and Commerce</p>
       <p className="text-lg mb-4">Previously I've interned at Stacker, Viget, and Parenthood Ventures</p>
       <p className="text-blue-500 mb-2">Here is my portfolio</p>
       <p className="text-blue-500 mb-2">Here is my resume</p>
