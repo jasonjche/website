12e35bc93405e0aa6659c5281beec59da8818d4b
Jason He <jasonhe.md@gmail.com>
2023-06-13 23:31:33 -0700

    Fix typescript issue

--- src/app/Card.tsx
+++ src/app/Card.tsx
@@ -1,4 +1,9 @@
-export function Card({ title, children }) {
+type CardProps = {
+  title?: string;
+  children: React.ReactNode;
+};
+
+export function Card({ title, children } : CardProps) {
   return (
     <div className="bg-gray-100 rounded-2xl p-4">
       {title && <h2 className="text-3xl font-bold">{title}</h2>}
