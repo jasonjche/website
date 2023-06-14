c0b09c88a32d852a3d84adcf2564027f93b88f25
Jason He <jasonhe.md@gmail.com>
2023-06-14 01:31:44 -0700

    Color changes

--- src/app/ContactCard.tsx
+++ src/app/ContactCard.tsx
@@ -4,13 +4,13 @@ import { FaTwitter, FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 const ContactCard = () => {
     return (
         <Card className="">
-            <div className="p-6 flex flex-row justify-between content-center text-4xl">
-              <a href="https://twitter.com/jasonjche" target="_blank" rel="noopener noreferrer" aria-label="Jason's Twitter" className="hover:text-[#1DA1F2] transition-colors duration-200 block"><FaTwitter /></a>
-              <a href="https://github.com/jasonjche" target="_blank" rel="noopener noreferrer" aria-label="Jason's Github" className="hover:text-purple-500 transition-colors duration-200 block"><FaGithub /></a>
-              <a href="https://www.linkedin.com/in/jasonjche" target="_blank" rel="noopener noreferrer" aria-label="Jason's LinkedIn" className="hover:text-[#0077B5] transition-colors duration-200 block"><FaLinkedin /></a>
-              <a href="mailto:jasonjche@virginia.edu" target="_blank" rel="noopener noreferrer" aria-label="Email Jason" className="hover:text-red-600 transition-colors duration-200 block"><FaEnvelope /></a>
+            <div className="p-6 flex flex-row justify-between items-center text-4xl">
+                <a href="https://twitter.com/jasonjche" target="_blank" rel="noopener noreferrer" aria-label="Jason's Twitter" className="hover:text-[#1DA1F2] transition-colors duration-200 block"><FaTwitter /></a>
+                <a href="https://github.com/jasonjche" target="_blank" rel="noopener noreferrer" aria-label="Jason's Github" className="hover:text-purple-500 transition-colors duration-200 block"><FaGithub /></a>
+                <a href="https://www.linkedin.com/in/jasonjche" target="_blank" rel="noopener noreferrer" aria-label="Jason's LinkedIn" className="hover:text-[#0077B5] transition-colors duration-200 block"><FaLinkedin /></a>
+                <a href="mailto:jasonjche@virginia.edu" target="_blank" rel="noopener noreferrer" aria-label="Email Jason" className="hover:text-red-600 transition-colors duration-200 block"><FaEnvelope /></a>
             </div>
-          </Card>
+        </Card>
     );
 };
 
--- src/app/ResumeCard.tsx
+++ src/app/ResumeCard.tsx
@@ -1,11 +1,31 @@
+'use client';
+
 import { Card } from './Card';
 
 const ResumeCard = () => {
+    const openProduct = () => {
+        window.open("https://www.dropbox.com/s/za6ptynh53kt8bc/Jason%20He%20Resume%20-%20Product.pdf?dl=0", "_blank", "noopener,noreferrer")
+    };
+    const openSoftware = () => {
+        window.open("https://chat.openai.com", "_blank", "noopener,noreferrer")
+    };
+
     return (
-        <Card className="">
-                <a href="https://www.dropbox.com/s/za6ptynh53kt8bc/Jason%20He%20Resume%20-%20Product.pdf?dl=0"
-                target="_blank" rel="noopener noreferrer" aria-label="Jason's Resume"
-                className="text-center hover:text-gray-500 transition-colors duration-200 block">Resume</a>
+        <Card className="flex gap-4">
+            <button
+                onClick={openProduct}
+                aria-label="Jason's PM Resume"
+                className="bg-[#878782] w-1/2 text-white px-4 py-2 rounded-xl text-center hover:bg-[#979792] transition-colors duration-200 block"
+            >
+                Product Resume
+            </button>
+            <button
+                onClick={openSoftware}
+                aria-label="Jason's SWE Resume"
+                className="bg-[#fff] text-black w-1/2 px-4 py-2 rounded-xl text-center hover:bg-[#eee] transition-colors duration-200 block"
+            >
+                SWE Resume
+            </button>
         </Card>
     );
 };
